7e0f394bfe74d7d60a85c43d6751de835c9e4538
Ali <a.a.ghanati@gmail.com>
2020-07-04 12:42:34 +0430

    'debug_falsed'

--- udemy_practice/settings.py
+++ udemy_practice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n3t3b&&akoa7!fjsbgf6dc@8(1(#_!m40zta57ujwzjnogp-z$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fletcher.pythonanywhere.com']
 
